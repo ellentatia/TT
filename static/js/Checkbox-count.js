6323ba78104ec587db50f3afb6d2fa3ff1ea78a1
Ellen <ellenguimaraes@gmail.com>
2019-08-12 16:45:46 +0000

    function screenshot

--- static/js/Checkbox-count.js
+++ static/js/Checkbox-count.js
@@ -28,3 +28,21 @@ document.addEventListener("DOMContentLoaded", function()
            dismissible:false
        });
  });
+
+$(".drag").draggable();
+$(".drag").droppable();
+var takeScreenShot = function() {
+    html2canvas(document.getElementById("container"), {
+        onrendered: function (canvas) {
+            var tempcanvas=document.createElement('canvas');
+            tempcanvas.width=350;
+            tempcanvas.height=350;
+            var context=tempcanvas.getContext('2d');
+            context.drawImage(canvas,112,0,288,200,0,0,350,350);
+            var link=document.createElement("a");
+            link.href=tempcanvas.toDataURL('image/jpg');   //function blocks CORS
+            link.download = 'screenshot.jpg';
+            link.click();
+        }
+    });
+}
