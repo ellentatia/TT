2f6cb85f28d8317fde9d8d907498b2ea46a94029
Ellen <ellenguimaraes@gmail.com>
2019-08-22 11:20:35 +0000

    % closer and dropped screenshot function

--- static/js/Checkbox-count.js
+++ static/js/Checkbox-count.js
@@ -15,7 +15,7 @@ function checkboxes()
             total= roundToTwo(count*4.2);
         }
       }
-     modalContent.innerHTML = total +" %";
+     modalContent.innerHTML = total +"%";
  }
 
 document.addEventListener("DOMContentLoaded", function()
@@ -29,21 +29,3 @@ document.addEventListener("DOMContentLoaded", function()
 
        });
  });
-
-$(".drag").draggable();
-$(".drag").droppable();
-var takeScreenShot = function() {
-    html2canvas(document.getElementById("container"), {
-        onrendered: function (canvas) {
-            var tempcanvas=document.createElement('canvas');
-            tempcanvas.width=350;
-            tempcanvas.height=350;
-            var context=tempcanvas.getContext('2d');
-            context.drawImage(canvas,112,0,288,200,0,0,350,350);
-            var link=document.createElement("a");
-            link.href=tempcanvas.toDataURL('image/jpg');   //function blocks CORS
-            link.download = 'screenshot.jpg';
-            link.click();
-        }
-    });
-}
